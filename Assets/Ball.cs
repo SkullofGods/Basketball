28d7aa844b47169398df49e23d3b55157714c78a
SkullofGods <salahtinov.ilua2010.ru@gmail.com>
2023-03-17 12:45:05 +0300

    Initial commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Ball : MonoBehaviour
+{
+    [HideInInspector] public Rigidbody2D rb;
+    [HideInInspector] public CircleCollider2D col;
+    public int Score;
+    public bool em = true;
+    public bool isGo;
+    public GameObject Hoop1;
+    public GameObject Hoop2;
+    public Text scoretext;
+    public Timer timerl;
+    [HideInInspector] public Vector3 pos { get { return transform.position; } }
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<CircleCollider2D>();
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Push(Vector2 force)
+    {
+        rb.AddForce(force, ForceMode2D.Impulse);
+    }
+
+    public void ActivateRb()
+    {
+        rb.isKinematic = false;
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        
+        Score++;
+        scoretext.text = Score.ToString();
+        if (em == true)
+        {
+          
+            Hoop1.SetActive(false);
+            Hoop2.SetActive(true);
+           
+        }
+        if (em == false)
+        {
+          
+            Hoop2.SetActive(false);
+            Hoop1.SetActive(true);
+           
+        }
+        em = !em;
+        timerl.MinusTime();
+       
+    }
+    public void DesactivateRb()
+    {
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = 0f;
+     
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGo = true;
+
+        }
+
+
+    }
+   
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,70 @@
+﻿ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine; 
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public GameObject buttan;
+    Image timerBar;
+    public GameObject pause;
+    public float maxTime = 150f;
+    bool notStarted = false;
+    public float timeLeft;
+
+
+    public void TapStart()
+    {
+        Time.timeScale = 1;
+        buttan.SetActive(false);
+        notStarted = true;
+    }
+
+
+    void Start() {
+
+        Time.timeScale = 0;
+        timerBar = GetComponent<Image>();
+
+        timeLeft = maxTime;
+
+    }
+
+
+    public void MinusTime()
+    {
+        if (maxTime > 5)
+        {
+            maxTime--;
+            timeLeft = maxTime;
+        }
+        else if (maxTime < 6)
+        {
+            timeLeft = maxTime;
+        }
+    }
+
+
+    void Update() {
+
+        if (notStarted == true) {
+            if (timeLeft > 0)
+            {
+
+                timeLeft -= 0.01f;
+
+                timerBar.fillAmount = timeLeft / maxTime;
+
+            }
+            else
+            {
+
+                Time.timeScale = 0;
+                pause.SetActive(true);
+            }
+        }
+    }
+}
+
+  
